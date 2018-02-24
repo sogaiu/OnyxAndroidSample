7012cdd6ba6401dd4363fa79b4bd4f08160fffcb
sogaiu <33044872+sogaiu@users.noreply.github.com>
2018-02-24 19:14:33 +0900

    Simplifiy PenStylusTouchHelperDemoActivity

--- app/sample/src/main/java/com/onyx/android/sample/PenStylusTouchHelperDemoActivity.java
+++ app/sample/src/main/java/com/onyx/android/sample/PenStylusTouchHelperDemoActivity.java
@@ -10,8 +10,6 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.RadioButton;
 
 import com.onyx.android.sdk.pen.RawInputCallback;
 import com.onyx.android.sdk.pen.TouchHelper;
@@ -23,33 +21,20 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 public class PenStylusTouchHelperDemoActivity extends AppCompatActivity {
 
     private static final String TAG = PenStylusTouchHelperDemoActivity.class.getSimpleName();
-    /** skip point count*/
-    private static final int INTERVAL = 10;
 
-    @Bind(R.id.button_pen)
-    Button buttonPen;
     @Bind(R.id.button_eraser)
     Button buttonEraser;
     @Bind(R.id.surfaceview)
     SurfaceView surfaceView;
-    @Bind(R.id.cb_render)
-    CheckBox cbRender;
-    @Bind(R.id.rb_brush)
-    RadioButton rbBrush;
-    @Bind(R.id.rb_pencil)
-    RadioButton rbPencil;
 
     private TouchHelper touchHelper;
 
     Paint paint = new Paint();
-    private TouchPoint startPoint;
-    private int countRec = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,10 +83,6 @@ public class PenStylusTouchHelperDemoActivity extends AppCompatActivity {
                 }
                 List<Rect> exclude = new ArrayList<>();
                 exclude.add(getRelativeRect(surfaceView, buttonEraser));
-                exclude.add(getRelativeRect(surfaceView, buttonPen));
-                exclude.add(getRelativeRect(surfaceView, cbRender));
-                exclude.add(getRelativeRect(surfaceView, rbBrush));
-                exclude.add(getRelativeRect(surfaceView, rbPencil));
 
                 Rect limit = new Rect();
                 surfaceView.getLocalVisibleRect(limit);
@@ -109,50 +90,18 @@ public class PenStylusTouchHelperDemoActivity extends AppCompatActivity {
                            .setLimitRect(limit, exclude)
                            .openRawDrawing();
                 touchHelper.setStrokeStyle(TouchHelper.STROKE_STYLE_BRUSH);
+                
+                touchHelper.setRawDrawingEnabled(true);
+                touchHelper.setRawDrawingRenderEnabled(true);
             }
         });
-    }
-
-    @OnClick(R.id.button_pen)
-    public void onPenClick() {
-        touchHelper.setRawDrawingEnabled(true);
-        onRenderEnableClick();
     }
 
     @OnClick(R.id.button_eraser)
     public void onEraserClick() {
         touchHelper.setRawDrawingEnabled(false);
         cleanSurfaceView();
-    }
-
-    @OnCheckedChanged(R.id.cb_render)
-    public void onRenderEnableClick() {
-        touchHelper.setRawDrawingRenderEnabled(cbRender.isChecked());
-        Log.d(TAG,"onRenderEnableClick setRawDrawingRenderEnabled =  " + cbRender.isChecked());
-    }
-
-    @OnClick({R.id.rb_brush, R.id.rb_pencil})
-    public void onRadioButtonClicked(RadioButton radioButton) {
-
-        boolean checked = radioButton.isChecked();
-        Log.d(TAG, radioButton.toString());
-        switch (radioButton.getId()) {
-            case R.id.rb_brush:
-                if (checked) {
-                    touchHelper.setStrokeStyle(TouchHelper.STROKE_STYLE_BRUSH);
-                    Log.d(TAG, "STROKE_STYLE_BRUSH");
-                }
-                break;
-            case R.id.rb_pencil:
-                if (checked) {
-                    touchHelper.setStrokeStyle(TouchHelper.STROKE_STYLE_PENCIL);
-                    Log.d(TAG, "STROKE_STYLE_PENCIL");
-                }
-                break;
-        }
-        // refresh ui
-        onEraserClick();
-        onPenClick();
+        touchHelper.setRawDrawingEnabled(true);
     }
 
     public Rect getRelativeRect(final View parentView, final View childView) {
@@ -179,52 +128,25 @@ public class PenStylusTouchHelperDemoActivity extends AppCompatActivity {
         return true;
     }
 
-    private void drawRect(TouchPoint endPoint){
-        Canvas canvas = surfaceView.getHolder().lockCanvas();
-        if (canvas == null ) {
-            return;
-        }
-
-        if (startPoint == null || endPoint == null) {
-            surfaceView.getHolder().unlockCanvasAndPost(canvas);
-            return;
-        }
-
-        canvas.drawColor(Color.WHITE);
-        canvas.drawRect(startPoint.getX(), startPoint.getY(), endPoint.getX(), endPoint.getY(), paint);
-        Log.d(TAG,"drawRect ");
-        surfaceView.getHolder().unlockCanvasAndPost(canvas);
-    }
-
     private RawInputCallback callback = new RawInputCallback() {
 
         @Override
         public void onBeginRawDrawing(boolean b, TouchPoint touchPoint) {
             Log.d(TAG, "onBeginRawDrawing");
-            startPoint = touchPoint;
-            Log.d(TAG,touchPoint.getX() +", " +touchPoint.getY());
+            Log.d(TAG,touchPoint.getX() + ", " + touchPoint.getY());
             countRec = 0;
         }
 
         @Override
         public void onEndRawDrawing(boolean b, TouchPoint touchPoint) {
             Log.d(TAG, "onEndRawDrawing");
-            if (!cbRender.isChecked()){
-                drawRect(touchPoint);
-            }
-            Log.d(TAG,touchPoint.getX() +", " +touchPoint.getY());
+            Log.d(TAG,touchPoint.getX() + ", " + touchPoint.getY());
         }
 
         @Override
         public void onRawDrawingTouchPointMoveReceived(TouchPoint touchPoint) {
             Log.d(TAG, "onRawDrawingTouchPointMoveReceived");
-            Log.d(TAG,touchPoint.getX() +", " +touchPoint.getY());
-            countRec++;
-            countRec = countRec % INTERVAL;
-            if (!cbRender.isChecked() && countRec == INTERVAL - 1 ){
-                drawRect(touchPoint);
-            }
-            Log.d(TAG,"countRec = " + countRec);
+            Log.d(TAG,touchPoint.getX() + ", " + touchPoint.getY());
         }
 
         @Override
@@ -235,16 +157,19 @@ public class PenStylusTouchHelperDemoActivity extends AppCompatActivity {
         @Override
         public void onBeginRawErasing(boolean b, TouchPoint touchPoint) {
             Log.d(TAG, "onBeginRawErasing");
+            Log.d(TAG,touchPoint.getX() + ", " + touchPoint.getY());
         }
 
         @Override
         public void onEndRawErasing(boolean b, TouchPoint touchPoint) {
             Log.d(TAG, "onEndRawErasing");
+            Log.d(TAG,touchPoint.getX() + ", " + touchPoint.getY());
         }
 
         @Override
         public void onRawErasingTouchPointMoveReceived(TouchPoint touchPoint) {
             Log.d(TAG, "onRawErasingTouchPointMoveReceived");
+            Log.d(TAG,touchPoint.getX() + ", " + touchPoint.getY());
         }
 
         @Override
